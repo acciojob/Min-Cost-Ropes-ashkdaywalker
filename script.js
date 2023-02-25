067ef15af0fd212ec417216a5ee29560dd0d9193
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-02-25 15:11:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+//your code here
+function minCostToFormRope(event)
+{
+	event.preventDefault();
+	
+	var inputElement=document.querySelector('input').value;
+	var arr=inputElement.split(',');
+	arr.sort(function(a,b){return a-b});
+	
+var cost=0;
+while(arr.length>1)
+{
+	var res=Number(arr[0])+Number(arr[1]);
+	arr.push(res);
+	cost+=res;
+	arr.shift();
+	arr.shift();
+	arr.sort(function (a,b){
+		return a-b
+		
+	});
 }
-
-module.exports=mincost;
+	document.getElementById("result").textContent=cost;
+}
